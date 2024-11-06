959fdec79afc3bc2a25c1e885a008b2264924ed4
Nazliiclal26 <156397510+Nazliiclal26@users.noreply.github.com>
2024-11-06 14:40:19 -0500

    Update movies.js

--- app/public/movies.js
+++ app/public/movies.js
@@ -1,16 +1,38 @@
-function code() {
-    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let code = '';
-    for (let i = 0; i < 10; i++) {
-        let rand = Math.floor(Math.random() * chars.length);
-        code += chars.charAt(rand);
+async function code() {
+    let code = prompt("Please enter a group code:");
+  
+    if (!code || code.trim() === '') {
+      alert("Invalid");
+      return;
     }
-    return code;
-}
+  
+    try {
+      let response = await fetch(`/codeValid`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ code })
+      });
+  
+      let result = await response.json();
+      if (result.isValid) {
+        alert("Valid ");
+        return code;
+      } 
+      else {
+        alert("Code already exists. Please try a different one.");
+        return code(); 
+      }
+    } catch (error) {
+      alert("Please try again.");
+    }
+  }
+  
 
 let createButton = document.getElementById("create");
-createButton.addEventListener("click", () => {
-    let groupCode = code(); 
+createButton.addEventListener("click", async () => {
+    let groupCode = await code(); 
+
+    if (!groupCode) return;
 
     let leaderId = localStorage.getItem('userId');
     if (!leaderId) {
