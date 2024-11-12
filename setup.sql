7b2383aca6b0b965837b95e5d1640a1836bf8a4a
Susie Choi <suschoi007@gmail.com>
2024-11-12 18:04:01 -0500

    removed created_at column from group_events

--- setup.sql
+++ setup.sql
@@ -73,8 +73,7 @@ CREATE TABLE group_events (
     group_code VARCHAR(255) NOT NULL,
     event_date DATE NOT NULL,
     event_title VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    description TEXT
 );
 
 
