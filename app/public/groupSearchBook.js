7e7add9635afec628b800012103ecb925e760393
Ajani Levere <arl349@drexel.edu>
2024-11-13 11:31:33 -0500

    Clear message after sending in chat

--- app/public/groupSearchBook.js
+++ app/public/groupSearchBook.js
@@ -158,6 +158,7 @@ document.addEventListener("DOMContentLoaded", async () => {
           console.log('Successful message addition. Now appending and sending to room:');
           appendSentMessage(message, username);
           socket.emit('sendMessageToRoom', { message, username });
+          input.value = '';
         }).catch((error) => {
           console.error(error);
         }); 
